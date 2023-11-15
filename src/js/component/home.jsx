f5b4a8f48339c7b1c7661aa39a33aadd5a032c59
juliangoaguirre <143230248+juliangoaguirre@users.noreply.github.com>
2023-11-15 20:32:04 +0000

    missing add and delete todos functions

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,18 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
+import React, { useState } from "react";
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [todoList, setTodoList] = useState(["example 1", "example 2"]);
+  const [inputValue, setInputValue] = useState("");
+  return (
+    <div className="text-center">
+      <h1>TO-DO LIST</h1>
+      <input type="text" placeholder="write your to-dos here" />
+      <div>
+        {todoList.map((item, index) => {
+          return <div>{item}</div>;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
